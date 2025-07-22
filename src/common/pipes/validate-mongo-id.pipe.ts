30916c32c6b266e802bf8a6c2893f1705b35a151
jmarqb <jacmelbalu7@gmail.com>
2025-07-22 03:30:25 -0400

    feat(common):add validate-mongo-id-pipe

---
+++ src/common/pipes/validate-mongo-id.pipe.ts
@@ -0,0 +1,11 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { Types } from 'mongoose';
+
+export class ValidateMongoIdPipe implements PipeTransform {
+  transform(value: string): string {
+    if (!Types.ObjectId.isValid(value)) {
+      throw new BadRequestException('Invalid MongoId');
+    }
+    return value;
+  }
+}
