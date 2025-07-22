45c453bedf41ffae82c31cc42ab8f8a97529a0be
jmarqb <jacmelbalu7@gmail.com>
2025-07-22 03:24:15 -0400

    feat(auth):add user-role guard

---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { User } from '../entities/user.entity';
+import { META_ROLES } from '../decorators';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get(
+      META_ROLES,
+      context.getHandler(),
+    );
+
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user)
+      throw new BadRequestException({
+        message: 'User not found',
+        reference: 'USER_NOT_FOUND',
+      });
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.firstName} need a valid role: [${validRoles}]`,
+    );
+  }
+}
