f06e70f417966702ce63cff6b4ca54b798d8f24b
jmarqb <jacmelbalu7@gmail.com>
2025-07-22 03:08:04 -0400

    feat(api):first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { envs } from './config';
+import { ProjectModule } from './project/project.module';
+import { ProjectUserModule } from './project-user/project-user.module';
+import { TaskModule } from './task/task.module';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(envs.databaseUrl),
+    ProjectModule,
+    ProjectUserModule,
+    TaskModule,
+    CommonModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const prefix: string = '/api';
+
+  app.setGlobalPrefix(prefix);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Projects Management API')
+    .setDescription('API for managing projects and tasks')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(envs.port);
+}
+
+bootstrap();
